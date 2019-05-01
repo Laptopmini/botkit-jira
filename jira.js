7072f15f4d05b9e09b50659ad0f5ffb58b22e033
Laptop_mini <laptopmini@gmail.com>
2019-05-01 14:38:24 -0700

    Prettier formatting

--- jira.js
+++ jira.js
@@ -1,57 +1,63 @@
 const utils = require('./utils');
 
 class UserNotFoundError extends Error {
-    constructor() {
-        super();
-        this.code = 404;
-    }
+  constructor() {
+    super();
+    this.code = 404;
+  }
 }
 
 async function getUser(key) {
-    const path = "/rest/api/2/user/search";
-    const parameters = {
-        username: key,
-        maxResults: 1
-    }
-    const response = await utils.get(path, parameters);
-    if (!response.length) {
-        throw new UserNotFoundError();
-    }
-    return response[0];
+  const path = '/rest/api/2/user/search';
+  const parameters = {
+    username: key,
+    maxResults: 1
+  };
+  const response = await utils.get(path, parameters);
+  if (!response.length) {
+    throw new UserNotFoundError();
+  }
+  return response[0];
 }
 
 async function isUserTeamMember(email) {
-    let user;
-    try {
-        user = await getUser(email);
-    } catch (error) {
-        if (error instanceof UserNotFoundError) {
-            console.log(`notice: User with email "${email}" not found, access was denied.`);
-            return false;
-        }
-        throw error;
+  let user;
+  try {
+    user = await getUser(email);
+  } catch (error) {
+    if (error instanceof UserNotFoundError) {
+      console.log(
+        `notice: User with email "${email}" not found, access was denied.`
+      );
+      return false;
     }
+    throw error;
+  }
 
-    const path = '/rest/api/2/user';
-    const parameters = {
-        username: user.name,
-        expand: 'groups'
+  const path = '/rest/api/2/user';
+  const parameters = {
+    username: user.name,
+    expand: 'groups'
+  };
+  const response = await utils.get(path, parameters);
+  if (response.groups !== undefined && response.groups.items !== undefined) {
+    const groups = response.groups.items;
+    for (var key in groups) {
+      const name = groups[key].name;
+      if (name !== undefined && name == 'Team All') {
+        return true;
+      }
     }
-    const response = await utils.get(path, parameters);
-    if (response.groups !== undefined && response.groups.items !== undefined) {
-        const groups = response.groups.items;
-        for (var key in groups) {
-            const name = groups[key].name;
-            if (name !== undefined && name == "Team All") {
-                return true;
-            }
-        }
-    }
-    console.log(`notice: User "${user.name}" does not belong to target group, access was denied.`);
-    return false;
+  }
+  console.log(
+    `notice: User "${
+      user.name
+    }" does not belong to target group, access was denied.`
+  );
+  return false;
 }
 
 module.exports = {
-    getUser,
-    isUserTeamMember
-}
+  getUser,
+  isUserTeamMember
+};
--- utils.js
+++ utils.js
@@ -1,77 +1,86 @@
 const request = require('request');
 
 function getAuthorizationHeaderValue() {
-    const user = process.env.JIRA_USER;
-    const key = process.env.JIRA_KEY;
-    if (user !== undefined && key !== undefined) {
-        return "Basic " + Buffer.from(user + ":" + key).toString('base64');
-    }
-    throw new Error("JIRA_USER and/or JIRA_KEY env variable is missing.");
+  const user = process.env.JIRA_USER;
+  const key = process.env.JIRA_KEY;
+  if (user !== undefined && key !== undefined) {
+    return 'Basic ' + Buffer.from(user + ':' + key).toString('base64');
+  }
+  throw new Error('JIRA_USER and/or JIRA_KEY env variable is missing.');
 }
 
 function encodeParameters(parameters, keys) {
-    const finalKeys = keys !== undefined ? keys : Object.keys(parameters);
-    return finalKeys.map(k => `${encodeURIComponent(k)}=${encodeURIComponent(parameters[k])}`).join('&');
+  const finalKeys = keys !== undefined ? keys : Object.keys(parameters);
+  return finalKeys
+    .map(k => `${encodeURIComponent(k)}=${encodeURIComponent(parameters[k])}`)
+    .join('&');
 }
 
 function isEmailOfSamsungPartner(email) {
-    return /@partner.samsung.com\s*$/.test(email);
+  return /@partner.samsung.com\s*$/.test(email);
 }
 
 function getUserEmail(bot, slackId) {
-    return new Promise((resolve, reject) => {
-        bot.api.users.info({user: slackId}, (error, response) => {
-            if (error) {
-                reject(error);
-                return;       
-            }
-            resolve(response.user.profile.email);
-        });
+  return new Promise((resolve, reject) => {
+    bot.api.users.info({ user: slackId }, (error, response) => {
+      if (error) {
+        reject(error);
+        return;
+      }
+      resolve(response.user.profile.email);
     });
+  });
 }
 
 function get(path, parameters = undefined) {
-    return new Promise((resolve, reject) => {
-        if (process.env.JIRA_HOST === undefined) {
-            reject(new Error("JIRA_HOST env variable is missing."));
-            return;
-        }
-        var authHeaderValue;
-        try {
-            authHeaderValue = getAuthorizationHeaderValue();
-        } catch (error) {
-            reject(error);
-            return;
-        }
-        
-        var finalUrl = process.env.JIRA_HOST + path;
-        if (parameters !== undefined) {
-            finalUrl += '?' + encodeParameters(parameters);
-        }
+  return new Promise((resolve, reject) => {
+    if (process.env.JIRA_HOST === undefined) {
+      reject(new Error('JIRA_HOST env variable is missing.'));
+      return;
+    }
+    var authHeaderValue;
+    try {
+      authHeaderValue = getAuthorizationHeaderValue();
+    } catch (error) {
+      reject(error);
+      return;
+    }
 
-        request.get({
-            url: finalUrl,
-            json: true,
-            headers: {
-                'Authorization': authHeaderValue,
-                'User-Agent': 'request'
-            }
-        }, (error, res, data) => {
-            if (error) {
-                reject(error);
-            } else if (res.statusCode !== 200) {
-                var err = new Error("Received an invalid status code from response. [" + res.statusCode + "]");
-                err.code = res.statusCode;
-                reject(err);
-            } else {
-                resolve(data);
-            }
-        });
-    });
+    var finalUrl = process.env.JIRA_HOST + path;
+    if (parameters !== undefined) {
+      finalUrl += '?' + encodeParameters(parameters);
+    }
+
+    request.get(
+      {
+        url: finalUrl,
+        json: true,
+        headers: {
+          Authorization: authHeaderValue,
+          'User-Agent': 'request'
+        }
+      },
+      (error, res, data) => {
+        if (error) {
+          reject(error);
+        } else if (res.statusCode !== 200) {
+          var err = new Error(
+            'Received an invalid status code from response. [' +
+              res.statusCode +
+              ']'
+          );
+          err.code = res.statusCode;
+          reject(err);
+        } else {
+          resolve(data);
+        }
+      }
+    );
+  });
 }
 
 module.exports = {
-    isEmailOfSamsungPartner,
-    getUserEmail,
-    get
-}
+  isEmailOfSamsungPartner,
+  getUserEmail,
+  get
+};
